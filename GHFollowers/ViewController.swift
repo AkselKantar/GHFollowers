162d2e58a63f2262cc7dd95c76faf24506231d31
Aksel Kantar <akselkantar@Aksels-MacBook-Pro.local>
2020-03-29 14:02:18 +0300

    initial commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Aksel Kantar on 29.03.2020.
+//  Copyright © 2020 Aksel Kantar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
